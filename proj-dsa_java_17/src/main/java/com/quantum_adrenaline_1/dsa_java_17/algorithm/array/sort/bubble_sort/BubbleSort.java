27d626e89114c6f482392e67b901979b3238c8a0
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2023-10-16 18:09:23 +0530

    Added javadoc for conventional Bubble Sort
    Added javadoc for the conventional Bubble Sort algorithm implementation in BubbleSort.java

--- proj-dsa_java_17/src/main/java/com/quantum_adrenaline_1/dsa_java_17/algorithm/array/sort/bubble_sort/BubbleSort.java
+++ proj-dsa_java_17/src/main/java/com/quantum_adrenaline_1/dsa_java_17/algorithm/array/sort/bubble_sort/BubbleSort.java
@@ -2,12 +2,26 @@ package com.quantum_adrenaline_1.dsa_java_17.algorithm.array.sort.bubble_sort;
 
 import java.util.Arrays;
 
+/**
+ * @implNote : sorting scheme to mention the sorting order preference in sorting
+ * @author : quantum-adrenaline-1
+ */
 enum SortingScheme {
     ASCENDING,
     DESCENDING
 }
 
+/**
+ * @implNote : conventional Bubble Sort implementation for both ascending and descending sorting preference
+ * @author : quantum-adrenaline-1
+ */
 public final class BubbleSort {
+    /**
+     * @implNote : conventional Bubble Sort implementation for both ascending and descending sorting preference
+     * @param userArray : the array to be sorted
+     * @param sortingScheme : the sorting scheme (sorting order) for Bubble Sort algorithm
+     * @author : quantum-adrenaline-1
+     */
     private static void bubbleSort(int[] userArray, SortingScheme sortingScheme) {
         if (sortingScheme == SortingScheme.ASCENDING) {
             for (int outerIndex = userArray.length - 1; outerIndex > 0; outerIndex--) {
@@ -32,6 +46,11 @@ public final class BubbleSort {
         }
     }
 
+    /**
+     * @implNote : 'main' method for implementing execution for 'Bubble Sort' implementation in this class
+     * @param args : 'runtime' string array arguments for main method
+     * @author : quantum-adrenaline-1
+     */
     public static void main(String[] args) {
         final int[] userArray = {5, 8, 6, 3, 2, 9, 1};
 
