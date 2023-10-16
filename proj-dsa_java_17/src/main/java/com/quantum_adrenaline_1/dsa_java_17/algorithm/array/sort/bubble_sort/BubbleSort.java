f99ee06e45cdc1edb340bdd83f04e1500734dc72
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-16 15:41:43 +0530

    Implemented Bubble Sort in descending order and a custom sorting scheme for sorting order preference
    Implemented Bubble Sort in descending order using a Sorting Scheme enum to customize order of sorting

--- proj-dsa_java_17/src/main/java/com/quantum_adrenaline_1/dsa_java_17/algorithm/array/sort/bubble_sort/BubbleSort.java
+++ proj-dsa_java_17/src/main/java/com/quantum_adrenaline_1/dsa_java_17/algorithm/array/sort/bubble_sort/BubbleSort.java
@@ -2,14 +2,31 @@ package com.quantum_adrenaline_1.dsa_java_17.algorithm.array.sort.bubble_sort;
 
 import java.util.Arrays;
 
+enum SortingScheme {
+    ASCENDING,
+    DESCENDING
+}
+
 public final class BubbleSort {
-    private static void bubbleSort(int[] userArray, String scheme) {
-        for (int outerIndex = userArray.length - 1; outerIndex > 0; outerIndex--) {
-            for (int innerIndex = 0; innerIndex < outerIndex; innerIndex++) {
-                if (userArray[innerIndex] > userArray[innerIndex + 1]) {
-                    int swapAccumulator = userArray[innerIndex];
-                    userArray[innerIndex] = userArray[innerIndex + 1];
-                    userArray[innerIndex + 1] = swapAccumulator;
+    private static void bubbleSort(int[] userArray, SortingScheme sortingScheme) {
+        if (sortingScheme == SortingScheme.ASCENDING) {
+            for (int outerIndex = userArray.length - 1; outerIndex > 0; outerIndex--) {
+                for (int innerIndex = 0; innerIndex < outerIndex; innerIndex++) {
+                    if (userArray[innerIndex] > userArray[innerIndex + 1]) {
+                        int swapAccumulator = userArray[innerIndex];
+                        userArray[innerIndex] = userArray[innerIndex + 1];
+                        userArray[innerIndex + 1] = swapAccumulator;
+                    }
+                }
+            }
+        } else if (sortingScheme == SortingScheme.DESCENDING) {
+            for (int outerIndex = 0; outerIndex < userArray.length - 1; outerIndex++) {
+                for (int innerIndex = userArray.length - 1; innerIndex > outerIndex; innerIndex--) {
+                    if (userArray[innerIndex] > userArray[innerIndex - 1]) {
+                        int swapAccumulator = userArray[innerIndex];
+                        userArray[innerIndex] = userArray[innerIndex - 1];
+                        userArray[innerIndex - 1] = swapAccumulator;
+                    }
                 }
             }
         }
@@ -18,7 +35,11 @@ public final class BubbleSort {
     public static void main(String[] args) {
         final int[] userArray = {5, 8, 6, 3, 2, 9, 1};
 
-        bubbleSort(userArray, "");
+        bubbleSort(userArray, SortingScheme.ASCENDING);
+
+        System.out.println(Arrays.toString(userArray));
+
+        bubbleSort(userArray, SortingScheme.DESCENDING);
 
         System.out.println(Arrays.toString(userArray));
     }
