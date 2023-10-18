644a114f0c29423dbde64aef096e5cb30b2f5f3d
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-18 19:04:50 +0530

    Created interfaces AscendingBubbleSort.java and DescendingBubbleSort.java
    Created interfaces AscendingBubbleSort.java and DescendingBubbleSort.java as an abstraction for the sorting schemes ascending and descending respectively in the implementation of the optimized Bubble Sort algorithm with sorting scheme choice using recursion

---
+++ proj-dsa_java_17/src/main/java/com/quantum_adrenaline_1/dsa_java_17/algorithm/array/sort/bubble_sort/efficient_with_sorting_scheme/AscendingBubbleSort.java
@@ -0,0 +1,9 @@
+package com.quantum_adrenaline_1.dsa_java_17.algorithm.array.sort.bubble_sort.efficient_with_sorting_scheme;
+
+import com.quantum_adrenaline_1.dsa_java_17.algorithm.array.sort.SortAlgorithm;
+
+public interface AscendingBubbleSort extends BubbleSort {
+
+    void ascendingBubbleSort(int[] array, int currentOuterIndex);
+
+}
---
+++ proj-dsa_java_17/src/main/java/com/quantum_adrenaline_1/dsa_java_17/algorithm/array/sort/bubble_sort/efficient_with_sorting_scheme/DescendingBubbleSort.java
@@ -0,0 +1,8 @@
+package com.quantum_adrenaline_1.dsa_java_17.algorithm.array.sort.bubble_sort.efficient_with_sorting_scheme;
+
+
+public interface DescendingBubbleSort extends BubbleSort {
+
+    void descendingBubbleSort(int[] array, int currentOuterIndex);
+
+}
